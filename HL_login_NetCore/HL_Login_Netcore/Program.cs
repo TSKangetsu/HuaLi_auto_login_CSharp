d1f41a0996a64a95f2c3a430823f10c6ad0d64d5
root <root@URANUS-TSKG.localdomain>
2019-04-12 22:41:17 +0800

    reset

---
+++ HL_login_NetCore/HL_Login_Netcore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HL_Login_Netcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
