60f9d8c3a64bae51a365daa1820571af976d84e8
TSKangetsu <x403070567@live.com>
2019-04-25 11:08:39 +0800

    dir reset

---
+++ HUALI_NETService/HUALI_NETService/Start.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace HUALI_NETObject
+{
+    class Detect_info
+    {
+        public string User1 { get; set; }
+        public string Password1 { get; set; }
+        public string inst_note { get; set; }
+        public string path1 = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/HUALI_login.txt";
+        public string Detect()
+        {
+            try
+            {
+                StreamReader str2 = new StreamReader(path1);
+                User1 = str2.ReadLine();
+                Password1 = str2.ReadLine();
+                inst_note = str2.ReadLine();
+            }catch
+            {
+                inst_note = "not installed";
+            }
+            return inst_note;
+        }
+    }
+
+    class Ride_info:Detect_info
+    {
+        public Ride_info()
+        {
+            string path = System.IO.Directory.GetCurrentDirectory();
+            String path1 = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/HUALI_login.txt";
+            if (File.Exists(path1) == false)
+            {
+                Console.WriteLine("请输入用户名");
+                User1 = Console.ReadLine();
+                Console.WriteLine("请输入密码");
+                Password1 = Console.ReadLine();
+                /*----------------------------------*/
+                FileStream txtfile = new FileStream(path1, FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter txtwrite = new StreamWriter(txtfile);
+                txtwrite.WriteLine(User1);
+                txtwrite.WriteLine(Password1);
+                txtwrite.WriteLine("installed");
+                txtwrite.Close();
+                /*---------------------------------*/
+            }
+        }
+    }
+}
